9b4c67c92266c72ba719477e206fe54590c612bd
Rcit17063388 <it17063388@my.sliit.lk>
2021-05-18 18:34:00 +0530

    Script page upto date.

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -9,7 +9,7 @@ var helloSpeaker = {};
 //helloSpeaker.name = "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim";
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello"
+var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
